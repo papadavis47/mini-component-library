f7abfca117b1a4c5a12959126d7e3b313a29990d
John William Davis <jwdavisdeveloper@gmail.com>
2021-10-27 13:15:15 -0700

    Initial work on progress bar - while watching video as codealong

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,28 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return (
+    <div
+      role='progressbar'
+      aria-valuenow={value}
+      aria-valuemin='0'
+      aria-valuemax='100'
+    >
+      <VisuallyHidden>{value}%</VisuallyHidden>
+      <Bar style={{ "--width": value + "%" }} />
+    </div>
+  );
 };
+
+const Bar = styled.div`
+  width: var(--width);
+  height: 8px;
+  background-color: ${COLORS.primary};
+`;
 
 export default ProgressBar;
